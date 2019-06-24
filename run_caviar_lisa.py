4a830c2968371654d059062d18c054654742508b
Shubham Saini <shubhamsaini@gmail.com>
2019-06-24 14:30:12 -0700

    Add caviar script to run on Lisa

--- run_caviar_lisa.py
+++ run_caviar_lisa.py
@@ -8,12 +8,13 @@ shubhamsaini@eng.ucsd.edu
 Run CAVIAR on SNP-STR GWAS results
 
 Example usage:
-./run_caviar_snorlax.py \
---vcf /storage/s1saini/hipstr_allfilters/str_snp/tredparse/chr6.str.snp.with.tredparse.vcf.gz \
---str-assoc /storage/s1saini/caviar/str_assoc/pgc.chr6.assoc \
---snp-assoc /storage/s1saini/caviar/snp_assoc/chr6.snps.metal.txt \
---snp-rsid /storage/s1saini/caviar/snp_assoc/chr6.pos.rsid.txt \
---chrom 6 --pos 170870996 \
+./run_caviar_lisa.py \
+--str-vcf /storage/s1saini/manuscript_strsnp/fig3/hipstr.1kg/eur/chr22/hipstr.chr22.eur.vcf.gz\
+--snp-vcf /storage/s1saini/caviar/snp_gt_files.list \
+--str-assoc /storage/s1saini/caviar/str_assoc/pgc.chr22.assoc \
+--snp-assoc /storage/s1saini/caviar/snp_assoc/chr22.snps.metal.txt \
+--snp-rsid /storage/s1saini/caviar/snp_assoc/chr22.pos.rsid.txt \
+--chrom 22 --pos 39975317 \
 --window-kb 100
 
 """
@@ -27,6 +28,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from subprocess import Popen, PIPE, DEVNULL
+from collections import defaultdict
 
 def PrintLine(text, f):
     f.write(text+"\n")
@@ -46,7 +48,8 @@ def str2bool(v):
 
 def main():
     parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("--vcf", help="VCF File for LD calculation", required=True, type=str)
+    parser.add_argument("--str-vcf", help="STR VCF file for LD calculation", required=True, type=str)
+    parser.add_argument("--snp-vcf", help="SNP VCF files list for LD calculation", required=True, type=str)
     parser.add_argument("--str-assoc", help="STR GWAS file from plinkSTR", required=True, type=str)
     parser.add_argument("--snp-assoc", help="SNP GWAS file from Metal", required=True, type=str)
     parser.add_argument("--snp-rsid", help="SNP GWAS file from Metal", required=True, type=str)
@@ -61,7 +64,13 @@ def main():
     if args.out: outf = open(args.out,"w")
     else: outf = sys.stdout
 
-    input_vcf = args.vcf
+    snp_vcf_files = []
+    with open(args.snp_vcf) as f:
+        for line in f:
+            snp_vcf_files.append(line.strip())
+
+    str_vcf_file = args.str_vcf
+
     CHROM = int(args.chrom)
     POS = int(args.pos)
     window=int(args.window_kb)*1000
@@ -74,6 +83,7 @@ def main():
     zfile = 'zscore_%d.csv'%(seed)
     numcausal = "2"
     outfile = "caviar.output_%d"%(seed)
+
     PROGRESS('Seed %d'%(seed))
     PROGRESS("Loading STR GWAS results")
 
@@ -104,42 +114,58 @@ def main():
     gwas_rsid = list(snp_assoc_result['ID'].values) + list(str_assoc_result['ID'].values)
 
 
-    PROGRESS("Loading genotypes")
-    vcf = VCF(input_vcf)
-    gt_array = []
-    snps_rsid = []
-    str_rsid = []
-    for v in vcf('%d:%d-%d'%(CHROM,START,END)):
-        if len(v.REF) == 1:
-            if str(v.ID) in gwas_rsid:
-                gt_array.append(list(np.sum(np.array(v.genotypes)[:,0:2], axis=1)))
-                snps_rsid.append(str(v.ID))
-        else:
-            str_gt = []
-            if str(v.ID) in gwas_rsid:
-                for gt in v.gt_bases:
-                    str_gt.append(np.sum([len(i)-len(v.REF) for i in gt.split("|")]))
-                gt_array.append(str_gt)
-                str_rsid.append(str(v.ID))
+    ### STR samples
+    vcf = VCF(str_vcf_file, gts012=True)
+    str_samples = vcf.samples
 
+    PROGRESS("Loading SNP genotypes")
+    gt_array = defaultdict(list)
+    snps_rsid = set()
+    str_rsid = set()
+    samples = []
+
+    for vcf_file_loc in snp_vcf_files:
+        vcf = VCF(vcf_file_loc, gts012=True, samples = str_samples)
+        samples = samples + list(vcf.samples)
+        for v in vcf('%d:%d-%d'%(CHROM,START,END)):
+            if len(v.REF) == 1 and str(v.ID) in gwas_rsid:
+                gt = np.array(v.gt_types, dtype="float")
+                gt[gt==3] = np.nan
+                gt_array[str(v.ID)] = gt_array[str(v.ID)] + list(gt)
+                snps_rsid.add(str(v.ID))
+
+    gt_array = pd.DataFrame(gt_array, dtype="float")
+
+    PROGRESS("Loading STR genotypes")
+    gt_array2 = defaultdict(list)
+
+    vcf = VCF(str_vcf_file, gts012=True, samples=samples)
+    for v in vcf('%d:%d-%d'%(CHROM,START,END)):
+        if str(v.ID) in gwas_rsid:
+            gt = np.array(v.gt_types, dtype="float")
+            gt[gt==3] = np.nan
+            gt_array2[str(v.ID)] = gt_array2[str(v.ID)] + list(gt)
+            str_rsid.add(str(v.ID))
+
+    gt_array2 = pd.DataFrame(gt_array2, dtype="float")
+
+    PROGRESS("Writing LD file")
+    full_geno_matrix = pd.concat([gt_array, gt_array2], axis=1)
+    full_geno_matrix = pd.DataFrame(full_geno_matrix, dtype="float")
+    corr_matrix = full_geno_matrix.corr().fillna(0)**2
+    corr_matrix_array = corr_matrix.values
+    np.savetxt(ldfile, corr_matrix_array, fmt='%.3f')
+    print(corr_matrix.shape)
+
+    PROGRESS("Writing Z-score file")
     pgc_snps_pval = snp_assoc_result
     pgc_snps = set(snps_rsid)
 
     pgc_strs_pval = str_assoc_result
     pgc_strs = set(str_rsid)
 
-    PROGRESS("Writing LD file")
-
-    df = pd.DataFrame(np.transpose(gt_array))
-    corr_matrix = df.corr().fillna(0)**2
-    corr_matrix = corr_matrix.values
-    np.savetxt(ldfile, corr_matrix, fmt='%.3f')
-    corr_matrix.shape
-
-    PROGRESS("Writing Z-score file")
-
     pgc_strs_pval["sign"] = np.sign(np.log(pgc_strs_pval['OR']))
-    pgc_strs_pval['Zscore'] = pgc_strs_pval["sign"]*abs(norm.ppf(pgc_strs_pval["P"]/2)) # 6.1
+    pgc_strs_pval['Zscore'] = pgc_strs_pval["sign"]*abs(norm.ppf(pgc_strs_pval["P"]/2))
     pgc_strs_pval = pgc_strs_pval[pgc_strs_pval.ID.isin(pgc_strs)]
     pgc_strs_pval = pgc_strs_pval[['BP','ID','Zscore','P','vtype']]
 
@@ -148,6 +174,8 @@ def main():
 
     str_snp = pd.concat([pgc_strs_pval,pgc_snps_pval])
     str_snp_caviar = str_snp.sort_values(by=['BP'])[['ID','Zscore']]
+    str_snp_caviar = str_snp_caviar.set_index("ID")
+    str_snp_caviar = str_snp_caviar.loc[list(corr_matrix.index)].reset_index()
     str_snp_caviar.to_csv(zfile, sep="\t", index=False, header=False)
 
 
