Caviar script now accepts number of causal variants as input. Fixed a bug where indels were not included in caviar analysis

run_caviar_lisa.py
@@ -13,7 +13,6 @@ Example usage:
 --snp-vcf /home/gymrek/ssaini/caviar/chr22.files.list \
 --str-assoc /home/gymrek/ssaini/gwas/assoc_results/str_assoc/pgc.chr22.assoc \
 --snp-assoc /home/gymrek/ssaini/gwas/assoc_results/snp_assoc/chr22.snps.metal.txt \
---snp-rsid /home/gymrek/ssaini/gwas/assoc_results/snp_assoc/chr22.pos.rsid.txt \
 --chrom 22 --pos 39975317 \
 --window-kb 10
 
@@ -58,8 +57,8 @@ def main():
     parser.add_argument("--str-vcf", help="STR VCF file for LD calculation", required=True, type=str)
     parser.add_argument("--snp-vcf", help="SNP VCF files list for LD calculation", required=True, type=str)
     parser.add_argument("--str-assoc", help="STR GWAS file from plinkSTR", required=True, type=str)
-    parser.add_argument("--snp-assoc", help="SNP GWAS file from Metal", required=True, type=str)
-    parser.add_argument("--snp-rsid", help="SNP GWAS file from Metal", required=True, type=str)
+    parser.add_argument("--snp-assoc", help="SNP GWAS file from Plink", required=True, type=str)
+    parser.add_argument("--caviar", help="Path to Caviar binary", required=False, type=str)
     parser.add_argument("--chrom", help="CHROM", required=True, type=str)
     parser.add_argument("--pos", help="POSITION", required=True, type=str)
     parser.add_argument("--window-kb", help="Window in KB", required=True, type=str)
@@ -67,6 +66,7 @@ def main():
     parser.add_argument("--min-snps", help="Minimum number of SNPs to include", required=False, type=int)
     parser.add_argument("--out-dir", help="Output directory name", required=False, type=str, default=".")
     parser.add_argument("--plot", help="Output top variants and plots", action="store_true")
+    parser.add_argument("--numcausal", help="Number of causal loci", required=False, type=str, default="2")
 
     parser.add_argument("--out", help="Output file name. Default: stdout", required=False, type=str)
     args = parser.parse_args()
@@ -75,10 +75,16 @@ def main():
     if args.out: outf = open(args.out,"w")
     else: outf = sys.stdout
 
+    if args.caviar:
+        CAVIAR_BIN = args.caviar
+    else:
+        CAVIAR_BIN = "/home/gymrek/ssaini/caviar/caviar/CAVIAR-C++/CAVIAR"
+
     snp_vcf_files = []
     with open(args.snp_vcf) as f:
         for line in f:
-            snp_vcf_files.append(line.strip())
+            if len(line)>0:
+                snp_vcf_files.append(line.strip())
 
     str_vcf_file = args.str_vcf
 
@@ -92,7 +98,7 @@ def main():
     seed = np.random.randint(100000)
     ldfile = '%s/corr_mat_%d_%d.txt'%(args.out_dir,CHROM, POS)
     zfile = '%s/zscore_%d_%d.csv'%(args.out_dir,CHROM, POS)
-    numcausal = "2"
+    numcausal = args.numcausal
     outfile = "%s/caviar.output_%d_%d"%(args.out_dir,CHROM, POS)
 
 
@@ -100,8 +106,8 @@ def main():
     PrintLine("Loading STR GWAS results", outf)
 
     str_assoc_result = pd.read_csv(args.str_assoc, delim_whitespace=True)
-    str_assoc_result = str_assoc_result[[str_assoc_result.columns.tolist()[i] for i in [0,1,3,2,6,4]]]
-    str_assoc_result.columns = ["CHR", "BP","P","ID","CI95","OR"]
+    str_assoc_result = str_assoc_result[["CHROM", "BP","SNP","P","CI95","OR"]]
+    str_assoc_result = str_assoc_result[str_assoc_result['CHROM']==CHROM]
     str_assoc_result = str_assoc_result[(str_assoc_result['BP']>=START) & (str_assoc_result['BP']<=END)]
     str_assoc_result = str_assoc_result[str_assoc_result.P != "NAN"]
     str_assoc_result['vtype'] = "STR"
@@ -116,12 +122,13 @@ def main():
 
     snp_assoc_result = pd.read_csv(args.snp_assoc, delim_whitespace=True)
     snp_assoc_result = snp_assoc_result[snp_assoc_result['TEST']=="ADD"]
+    snp_assoc_result = snp_assoc_result[snp_assoc_result['#CHROM']==CHROM]
     snp_assoc_result = snp_assoc_result[["POS","P","ID","Z_STAT"]]
     snp_assoc_result.columns = ["BP","P","ID","Zscore"]
     snp_assoc_result['BP'] = snp_assoc_result.BP.astype('int')
     snp_assoc_result = snp_assoc_result[(snp_assoc_result['BP']>=START) & (snp_assoc_result['BP']<=END)]
     snp_assoc_result['vtype'] = "SNP"
-    snp_assoc_result['CHR'] = CHROM
+    snp_assoc_result['CHROM'] = CHROM
     snp_assoc_result['P'] = snp_assoc_result.P.astype('float')
     if args.maxp:
         if args.min_snps and snp_assoc_result[snp_assoc_result['P']<=float(args.maxp)].shape[0] < args.min_snps:
@@ -132,11 +139,11 @@ def main():
     snp_assoc_result = snp_assoc_result.sort_values(by=['BP'])
     print(snp_assoc_result.shape)
 
-    gwas_rsid = list(snp_assoc_result['ID'].values) + list(str_assoc_result['ID'].values)
+    gwas_rsid = list(snp_assoc_result['ID'].values) + list(str_assoc_result['SNP'].values)
 
 
     ### STR samples
-    vcf = VCF(str_vcf_file, gts012=True)
+    vcf = VCF(str_vcf_file)
     str_samples = vcf.samples
 
 
@@ -144,6 +151,7 @@ def main():
 
     for vcf_file_loc in snp_vcf_files:
         cmd = f"bcftools query -r {region} -f '%ID\n' {vcf_file_loc} >> {args.out_dir}/snp_files_rsid_{CHROM}_{POS}.txt"
+        print(cmd)
         p = Popen(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
         output = p.communicate()[0]
         if p.returncode != 0:
@@ -168,13 +176,12 @@ def main():
 
     for vcf_file_loc in snp_vcf_files:
         vcf_rsid = set()
-        vcf = VCF(vcf_file_loc, gts012=True, samples = str_samples)
+        vcf = VCF(vcf_file_loc, samples = str_samples)
         for v in vcf('%d:%d-%d'%(CHROM,START,END)):
             vcf_rsid.add(str(v.ID))
-            if len(v.REF) == 1 and str(v.ID) in gwas_rsid:
-                gt = np.array(v.gt_types, dtype="float")
-                gt[gt==3] = np.nan
-                gt_array[str(v.ID)] = gt_array[str(v.ID)] + list(gt)
+            if str(v.ID) in gwas_rsid and str(v.ID) not in gt_array:
+                gt = [i[0]+i[1] if (i[0]!=-1 and len(i)==3) else np.nan for i in v.genotypes]
+                gt_array[str(v.ID)] = gt_array[str(v.ID)] + gt
                 snps_rsid.add(str(v.ID))
 
         samples = samples + list(vcf.samples)
@@ -182,30 +189,35 @@ def main():
         for rsid in not_found_rsid:
             gt_array[rsid] = gt_array[rsid] + [np.nan]*len(vcf.samples)
 
-    for i in gt_array.keys():
+    for i in list(gt_array):
         if len(gt_array[i]) == 0:
             del gt_array[i]
 
-    #print([len(gt_array[i]) for i in gt_array.keys()])
     gt_array = pd.DataFrame(gt_array, dtype="float")
+    gt_array['sample'] = samples
     print(gt_array.shape)
 
     PrintLine("Loading STR genotypes",outf)
     gt_array2 = defaultdict(list)
+    str_samples = []
 
-    vcf = VCF(str_vcf_file, gts012=True, samples=samples)
+    vcf = VCF(str_vcf_file, samples=samples)
     for v in vcf('%d:%d-%d'%(CHROM,START,END)):
         if str(v.ID) in gwas_rsid:
-            gt = np.array(v.gt_types, dtype="float")
-            gt[gt==3] = np.nan
-            gt_array2[str(v.ID)] = gt_array2[str(v.ID)] + list(gt)
+            gt = [(len(i.split("|")[0]) + len(i.split("|")[1])) - 2*len(v.REF) if "." not in i else np.nan for i in v.gt_bases]
+            gt_array2[str(v.ID)] = gt_array2[str(v.ID)] + gt
             str_rsid.add(str(v.ID))
 
+    str_samples = str_samples + list(vcf.samples)
     gt_array2 = pd.DataFrame(gt_array2, dtype="float")
+    gt_array2['sample'] = str_samples
     print(gt_array2.shape)
 
     PrintLine("Writing LD file", outf)
-    full_geno_matrix = pd.concat([gt_array, gt_array2], axis=1)
+    #full_geno_matrix = pd.concat([gt_array, gt_array2], axis=1)
+    full_geno_matrix = pd.merge(gt_array, gt_array2, on="sample")
+    full_geno_matrix.to_csv("%s/geno_chr%d-%d-%d.csv"%(args.out_dir,CHROM,START,END))
+    full_geno_matrix = full_geno_matrix.drop(['sample'], axis=1)
     full_geno_matrix = pd.DataFrame(full_geno_matrix, dtype="float")
     corr_matrix = full_geno_matrix.corr()#.fillna(0)
     notnan_ids = list(corr_matrix.columns[np.logical_not(corr_matrix.isna().all().values)])
@@ -223,8 +235,9 @@ def main():
 
     pgc_strs_pval["sign"] = np.sign(np.log(pgc_strs_pval['OR']))
     pgc_strs_pval['Zscore'] = pgc_strs_pval["sign"]*abs(norm.ppf(pgc_strs_pval["P"]/2))
-    pgc_strs_pval = pgc_strs_pval[pgc_strs_pval.ID.isin(pgc_strs)]
-    pgc_strs_pval = pgc_strs_pval[['BP','ID','Zscore','P','vtype']]
+    pgc_strs_pval = pgc_strs_pval[pgc_strs_pval.SNP.isin(pgc_strs)]
+    pgc_strs_pval = pgc_strs_pval[['BP','SNP','Zscore','P','vtype']]
+    pgc_strs_pval.columns = ['BP','ID','Zscore','P','vtype']
 
     pgc_snps_pval = pgc_snps_pval[pgc_snps_pval.ID.isin(pgc_snps)]
     pgc_snps_pval = pgc_snps_pval[['BP','ID','Zscore','P','vtype']]
@@ -237,7 +250,7 @@ def main():
 
 
     PrintLine("Running CAVIAR", outf)
-    cmd = "/home/gymrek/ssaini/caviar/caviar/CAVIAR-C++/CAVIAR -o %s -l %s -z %s -c %s"%(outfile, ldfile, zfile, numcausal)
+    cmd = "%s -o %s -l %s -z %s -c %s"%(CAVIAR_BIN, outfile, ldfile, zfile, numcausal)
     p = Popen(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
     output = p.communicate()[0]
     if p.returncode != 0:
@@ -253,10 +266,10 @@ def main():
         caviar_output['index'] = caviar_output.index
 
         print("============ Top Causal Variants ============")
-        print(caviar_output.head())
+        print(caviar_output.head().to_string())
 
         print("============ Top STR Variants ============")
-        print(caviar_output[caviar_output['vtype']=="STR"].head())
+        print(caviar_output[caviar_output['vtype']=="STR"].head().to_string())
 
         fig_df = caviar_output.copy()
         fig_df = fig_df[['index','BP','P','vtype']]